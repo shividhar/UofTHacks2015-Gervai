05cc0c626513ecaafc4c3473a7a37b83121e114a
Shivansh Dhar <shivanshdhar@gmail.com>
2015-01-31 18:22:59 -0500

    t

---
+++ server/publications/userPublication.js
@@ -0,0 +1,12 @@
+if(Meteor.isServer){
+	//Used to make sure Meteor.user() is not undefined
+	Meteor.publish("userSettings", function(){
+		if(this.userId){
+			//Find how to only return one field
+			return Meteor.users.find({_id : this.userId});
+		}else{
+			this.ready();
+		}
+	})
+
+}
