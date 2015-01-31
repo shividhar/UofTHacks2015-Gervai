744f390af08974fefc038c76b9c3b24f0ab30d14
Shivansh Dhar <shivanshdhar@gmail.com>
2015-01-31 14:20:12 -0500

    Changed schemam for questions and signupu complete method

--- client/scripts/completeSignupScript.js
+++ client/scripts/completeSignupScript.js
@@ -6,13 +6,15 @@ Template.completesignup.events({
 			firstName = t.find('####ID for firstName####').value,
 			lastName = t.find('####ID for lastName####').value,
 			age = t.find("####ID for age####").value,
-			gender = t.find("###ID for gender").value;
+			gender = t.find("###ID for gender").value,
+			mothersName = t.find("###Id for mothersName").value,
+			fathersName = t.find("###Id for fathersName").value;
 
 			var userNameSubscribe = Meteor.subscribe("userName", userName);
 
 			if(userNameSubscribe.ready()){
 				if(!Meteor.users.findOne({"userName": userName})){
-					Meteor.call("completeSignup", {firstName: firstName, lastName: lastName, age: age, gender: gender}, function(err){
+					Meteor.call("completeSignup", {firstName: firstName, lastName: lastName, age: age, gender: gender, mothersName: mothersName, fathersName: fathersName}, function(err){
 						if(err) {return alert(err.message)}
 						Router.go("homePage");
 					})
--- server/methods/completeSignupMethods.js
+++ server/methods/completeSignupMethods.js
@@ -11,12 +11,26 @@ if(Meteor.isServer){
 					var fullName = firstName + " " + lastName;
 					var age = signupCompleteData.age;
 					var gender = signupCompleteData.gender;
-					if(!!userName && !!firstName && !!lastName && !!fullName && !!age && !!gender){
-		    			Meteor.users.update(Meteor.userId(), {$set: {userName: userName, firstName: firstName, lastName: lastName, fullName: fullName, age: age, gender: gender, "signupComplete": true } });
-		    			
-		    		}else{
-		    			throw new Meteor.Error("Please fill the field properly");
-		    		}
+					var mothersName = signupCompleteData.mothersName;
+					var fathersName = signupCompleteData.fathersName;
+					if(userName.indexOf("'") != 1 && firstName.indexOf("'") != 1 && lastName.indexOf("'") != 1 && !isNaN(age) && mothersName.indexOf("'") != 1 && fathersName.indexOf("'") != 1){
+						if(!!userName && !!firstName && !!lastName && !!fullName && !!age && !!gender){
+			    			Meteor.users.update(Meteor.userId(), {$set: {userName: userName, firstName: firstName, lastName: lastName, fullName: fullName, age: age, gender: gender, "signupComplete": true } });
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Is your first name " + lastName + "?", correctAnswer: false});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Are you " + (age - 7) + " years old?", correctAnswer: false});
+			    			Questions.insert({"authorId": Meteor.userId(),question: "Is your first name " + firstName + "?", correctAnswer: true});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Is your last name " + lastName + "?", correctAnswer: true});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Is your full name " + lastName + " " + firstName + "?", correctAnswer: false});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Do you love " + mothersName + "?", correctAnswer: true});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Is " + fathersName + " your mother" + "?", correctAnswer: false});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Are you " + age + " years old?", correctAnswer: true});
+			    			Questions.insert({"authorId": Meteor.userId(), question: "Is you last name " + fullName + " years old?", correctAnswer: false});
+			    		}else{
+			    			throw new Meteor.Error("Please fill the field properly");
+			    		}
+					}else{
+						throw new Meteor.Error("Please don't use any apostrophes");
+					}
 	    		}
 	    	}
 		}
