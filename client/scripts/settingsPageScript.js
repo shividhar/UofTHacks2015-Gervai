520a8b401e8c14e9b4f31e8d2bfa76cb1fcb3b7c
Shivansh Dhar <shivanshdhar@gmail.com>
2015-02-01 11:03:59 -0500

    yeah

--- client/scripts/settingsPageScript.js
+++ client/scripts/settingsPageScript.js
@@ -15,7 +15,7 @@ Template.settingsPage.events({
 	'submit form[name=questions]': function(event){
 		var question = $("input[name=question]").val();
 		var correctAnswer = $("#truefalse").val() === "true";
-		
+
 		Meteor.call("addQuestion", {"question": question, "correctAnswer": correctAnswer}, function(err){
 			if(err) {return(alert(err.message))}
 			$("#question").val("");
@@ -27,3 +27,35 @@ Template.settingsPage.events({
 		
 	}
 })
+
+Template.settingsPage.rendered = function(){
+	function saveAccount(){
+        var options = {
+          'username':     $("#username").val(),
+          'password':     $("#password").val(),
+        };
+        return options;
+      };
+        
+      
+       if (typeof window.localStorage !== "undefined") {
+        console.log("Not undefined :)");
+        if (window.localStorage.bops) {
+          ls_pto = JSON.parse(window.localStorage.bops);
+          $("input[name=key0]").val(ls_pto["#username"]);
+          $("input[name=key1]").val(ls_pto["#password"]);
+        }
+       }
+        function sendOps() {
+          console.log("Submit");
+          ls_pto = JSON.stringify(saveOptions());
+          if (typeof window.localStorage !== "undefined") {
+            window.localStorage.bops = ls_pto;
+          }
+          var location = "pebblejs://close#" + ls_pto;
+          console.log("Warping to: " + location);
+          console.log(location);
+          document.location = location;
+        };
+
+    };
